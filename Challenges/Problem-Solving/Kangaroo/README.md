8e5d143e5cf06a380c656d4d0917228e5f2d254b
GabrielNBDS <gabriel.nbds@gmail.com>
2020-06-12 17:55:33 -0400

    Added Kangaroo

---
+++ Challenges/Problem-Solving/Kangaroo/README.md
@@ -0,0 +1,7 @@
+# Kangaroo
+
+Problema no HackerRank <a href="https://www.hackerrank.com/challenges/kangaroo/problem"> aqui </a>.
+</br>
+Explicação do problema <a href="./Problem.pdf"> aqui </a>.
+</br>
+Problema resolvido <a href="./submission.js"> aqui </a>.
---
+++ Challenges/Problem-Solving/Kangaroo/submission.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the kangaroo function below.
+function kangaroo(x1, v1, x2, v2) {
+  let current1 = x1;
+  let current2 = x2;
+  for(let i = 0; i <= 999999; i++) {
+    if( current1 === current2) {
+      return('YES');
+    }
+    current1 += v1;
+    current2 += v2;
+  }
+  return('NO');
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const x1V1X2V2 = readLine().split(' ');
+
+    const x1 = parseInt(x1V1X2V2[0], 10);
+
+    const v1 = parseInt(x1V1X2V2[1], 10);
+
+    const x2 = parseInt(x1V1X2V2[2], 10);
+
+    const v2 = parseInt(x1V1X2V2[3], 10);
+
+    let result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
